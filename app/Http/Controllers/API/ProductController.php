7b9912cb2d98935eb4816aca4cef357fa5c5a197
asail214 <asail.m.h.a.99@gmail.com>
2025-07-20 12:09:26 +0400

    Add API

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return response()->json(['message' => 'API Product list']);
+    }    //
+}
